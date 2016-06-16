a643c7357a014e15eb47fc58d6503bb1fc8d966e
Andrea Galloni <kingokongo46@gmail.com>
2016-06-16 16:40:23 +0000

    fixed start

--- start.sh
+++ start.sh
@@ -3,9 +3,10 @@
 sudo killall server.pl
 sudo killall dump.pl
 sudo killall airodump-ng
-./server.pl -p 7777 &
-./server.pl -p 7778 &
-./server.pl -p 7779 &
-./server.pl -p 7780 &
-./server.pl -p 7781 &
-#sudo ./dump.pl
+sudo ./server.pl -p 7777 &
+#./server.pl -p 7778 &
+#./server.pl -p 7779 &
+#./server.pl -p 7780 &
+#./server.pl -p 7781 &
+sleep 2
+sudo ./dump.pl
